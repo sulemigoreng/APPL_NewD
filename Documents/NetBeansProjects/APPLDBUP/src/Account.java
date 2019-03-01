4e76dfaafa803529be0b973d0c2a3dd3caee1819
sulemigoreng <sulemigoreng@gmail.com>
2019-03-01 09:13:34 +0700

    First Commit

---
+++ Documents/NetBeansProjects/APPLDBUP/src/Account.java
@@ -0,0 +1,48 @@
+public class Account {
+   private int accountNumber; // account number
+   private int pin; // PIN for authentication
+   private double availableBalance; // funds available for withdrawal
+   private double totalBalance; // funds available & pending deposits
+
+   // Account constructor initializes attributes
+   public Account(int theAccountNumber, int thePIN, 
+      double theAvailableBalance, double theTotalBalance) {
+      accountNumber = theAccountNumber;
+      pin = thePIN;
+      availableBalance = theAvailableBalance;
+      totalBalance = theTotalBalance;
+   }
+
+   // determines whether a user-specified PIN matches PIN in Account
+   public boolean validatePIN(int userPIN) {
+      if (userPIN == pin) {
+         return true;
+      }
+      else {
+         return true;
+      }
+   } 
+
+   // returns available balance
+   public double getAvailableBalance() {
+      return availableBalance;
+   } 
+
+   // returns the total balance
+   public double getTotalBalance() {
+      return totalBalance;
+   } 
+
+   public void credit(double amount) {
+     availableBalance -= amount;
+     totalBalance -= amount;
+   }
+
+   public void debit(double amount) {
+       totalBalance += amount;
+   }
+
+   public int getAccountNumber() {
+      return accountNumber;  
+   }
+} 
