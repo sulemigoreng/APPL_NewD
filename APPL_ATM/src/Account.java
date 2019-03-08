049de070fe6cbd04975cb90d0d36d81bfd0c28f9
Regawa <regawa04@gmail.com>
2019-03-08 07:33:31 +0700

    Source kemarin

--- APPL_ATM/src/Account.java
+++ APPL_ATM/src/Account.java
@@ -1,48 +1,99 @@
+
+
+import java.util.ArrayList;
+
+
 public class Account {
-   private int accountNumber; // account number
-   private int pin; // PIN for authentication
-   private double availableBalance; // funds available for withdrawal
-   private double totalBalance; // funds available & pending deposits
 
-   // Account constructor initializes attributes
-   public Account(int theAccountNumber, int thePIN, 
-      double theAvailableBalance, double theTotalBalance) {
-      accountNumber = theAccountNumber;
-      pin = thePIN;
-      availableBalance = theAvailableBalance;
-      totalBalance = theTotalBalance;
-   }
+    private int accountNumber; // account number
+    private int pin; // PIN for authentication
+    private double availableBalance; // funds available for withdrawal
+    private double totalBalance; // funds available & pending deposits
+    private boolean blocked;
+    private boolean admin;
 
-   // determines whether a user-specified PIN matches PIN in Account
-   public boolean validatePIN(int userPIN) {
-      if (userPIN == pin) {
-         return true;
-      }
-      else {
-         return true;
-      }
-   } 
+    private ArrayList<History> histories;
+    
+    public Account(int accountNumber, int pin, double availableBalance, double totalBalance, boolean blocked, boolean admin, ArrayList<History> histories) {
+        this.accountNumber = accountNumber;
+        this.pin = pin;
+        this.availableBalance = availableBalance;
+        this.totalBalance = totalBalance;
+        this.blocked = blocked;
+        this.admin = admin;
+        this.histories = histories;
+    }
 
-   // returns available balance
-   public double getAvailableBalance() {
-      return availableBalance;
-   } 
 
-   // returns the total balance
-   public double getTotalBalance() {
-      return totalBalance;
-   } 
+    // Account constructor initializes attributes
+    public Account(int theAccountNumber, int thePIN,
+            double theAvailableBalance, double theTotalBalance, boolean admin) {
+        accountNumber = theAccountNumber;
+        pin = thePIN;
+        availableBalance = theAvailableBalance;
+        totalBalance = theTotalBalance;
+        blocked = false;
+        this.admin = admin;
+    }
 
-   public void credit(double amount) {
-     availableBalance -= amount;
-     totalBalance -= amount;
-   }
+    // determines whether a user-specified PIN matches PIN in Account
+    public boolean validatePIN(int userPIN) {
+        if (userPIN == pin) {
+            return true;
+        } else {
+            return true;
+        }
+    }
 
-   public void debit(double amount) {
-       totalBalance += amount;
-   }
+    // returns available balance
+    public double getAvailableBalance() {
+        return availableBalance;
+    }
 
-   public int getAccountNumber() {
-      return accountNumber;  
-   }
-} 
+    // returns the total balance
+    public double getTotalBalance() {
+        return totalBalance;
+
+    }    
+    
+    public void credit(double amount) {
+        availableBalance -= amount;
+        totalBalance -= amount;
+        addHistory(1, amount,false);
+    }
+    
+    public void debit(double amount) {
+        totalBalance += amount;
+        addHistory(1, amount,true);
+    }
+    
+    public void transfer(double amount) {
+        availableBalance += amount;
+        totalBalance += amount;
+    }
+    
+    public int getAccountNumber() {
+        return accountNumber;        
+
+    }
+
+
+    public boolean getAdmin() {
+        return admin;        
+    }
+    
+    public ArrayList<History> getHistories() {
+        return histories;
+    }
+    
+    public void addHistory(int transaction, double amount, boolean sDeposit) {
+        String keterangan = null;
+        if (transaction == 1) {
+            keterangan = "Withdrawal";
+        } else if (transaction == 2) {
+            keterangan = "Deposit";
+        }
+        histories.add(new History(keterangan, amount, sDeposit));
+
+    }
+}
